a506985080f04f1e1b6290439215fa2b8d20ee16
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-04 22:57:15 +0530

    Stack DSA

---
+++ threeMainFunction.java
@@ -0,0 +1,11 @@
+package StackDS;
+ import java.util.Stack;
+public class threeMainFunction{
+    static class node{
+        int data;
+        node next;
+        node(int data){
+            this.data=data;
+        }
+    }
+}
