4b01bda43ff3be75416711429510882ae03ec6e6
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-05 13:58:50 +0530

    Stack DSA

--- threeMainFunction.java
+++ threeMainFunction.java
@@ -3,17 +3,31 @@ package StackDS;
 import java.util.Stack;
 
 public class threeMainFunction {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+
+            next = null;
+        }
+    }
+
+    static class Stacking {
+
+    }
 
     public static void main(String[] args) {
-        Stack s=new Stack<>();
+        Stack s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
         s.push(4);
-        while(!s.isEmpty()){
+        while (!s.isEmpty()) {
             System.out.println(
 
-            s.peek()
+                    s.peek()
 
             );
             s.pop();
