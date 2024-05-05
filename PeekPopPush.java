361681e8a14d60d8b97f19372ebba959bd962613
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-05 14:20:06 +0530

    Stack DSA

---
+++ PeekPopPush.java
@@ -0,0 +1,15 @@
+package StackDS;
+
+import java.util.Stack;
+
+public class PeekPopPush {
+    static class nodeing {
+        int data;
+        nodeing next;
+
+        nodeing(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+}
